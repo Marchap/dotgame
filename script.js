0889026ee48bbc4d8ddfecf649a52323ea946822
Marshall Chappell <marshall_chappell@hotmail.com>
2014-08-01 19:50:50 -0500

    Added score&color change options

--- script.js
+++ script.js
@@ -7,12 +7,6 @@ function dot(){
 	this.lside=0;
 }
 
-//used to list the available dots around the selected dot
-function available_dot(){x
-	this.side="";
-	this.tag="";
-}
-
 //create table
 function create_T(num){		
 	var board = new Array(num);
@@ -48,15 +42,15 @@ function check_open(dots){
 	}
 	$("#font_color").html("check_open: "+dots.tag+","+sides)
 	switch(dots.tag){
-		case '#a1':if(sides!=2){return 1;break;}else{return 0;}
-		case '#a2':if(sides!=3){return 1;break;}else{return 0;}
-		case '#a3':if(sides!=2){return 1;break;}else{return 0;}
-		case '#b1':if(sides!=3){return 1;break;}else{return 0;}
-		case '#b2':if(sides!=4){return 1;break;}else{return 0;}
-		case '#b3':if(sides!=3){return 1;break;}else{return 0;}
-		case '#c1':if(sides!=2){return 1;break;}else{return 0;}
-		case '#c2':if(sides!=3){return 1;break;}else{return 0;}
-		case '#c3':if(sides!=2){return 1;break;}else{return 0;}
+		case '#a1':if(sides!=2){return 1;break;}else{$('#a1').css('cursor','default');return 0;}
+		case '#a2':if(sides!=3){return 1;break;}else{$('#a2').css('cursor','default');return 0;}
+		case '#a3':if(sides!=2){return 1;break;}else{$('#a3').css('cursor','default');return 0;}
+		case '#b1':if(sides!=3){return 1;break;}else{$('#b1').css('cursor','default');return 0;}
+		case '#b2':if(sides!=4){return 1;break;}else{$('#b2').css('cursor','default');return 0;}
+		case '#b3':if(sides!=3){return 1;break;}else{$('#b3').css('cursor','default');return 0;}
+		case '#c1':if(sides!=2){return 1;break;}else{$('#c1').css('cursor','default');return 0;}
+		case '#c2':if(sides!=3){return 1;break;}else{$('#c2').css('cursor','default');return 0;}
+		case '#c3':if(sides!=2){return 1;break;}else{$('#c3').css('cursor','default');return 0;}
 	}
 }
 
@@ -70,217 +64,202 @@ function find_available(dots){
 	//blank array to house the id's of available dots
 	available = new Array();
 	//temporary variables
-	var aside ="";
-	var atag = "";
-			if(dots.charAt(1)=="a"){
-				if(dots.charAt(2)=="1"){
-				 	if(board[0][1].lside==0){
-				 		$("#a2").css('opacity',.5);
-				 		aside ="lside";
-				 		atag ="#a2";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[1][0].upper==0){
-				 		$("#b1").css('opacity',.5);
-				 		aside ="upper";
-				 		atag ="#b1";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	return 1;
-				}
-				else if(dots.charAt(2)=="2"){
-					if(board[0][0].rside==0){
-				 		$("#a1").css('opacity',.5);
-				 		aside ="rside";
-				 		atag ="#a1";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[0][2].lside==0){
-				 		$("#a3").css('opacity',.5);
-				 		aside ="lside";
-				 		atag ="#a3";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[1][1].upper==0){
-				 		$("#b2").css('opacity',.5);
-				 		aside ="upper";
-				 		atag ="#b2";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	return 1;
-				}
-				else if(dots.charAt(2)=="3"){
-					if(board[0][1].rside==0){
-				 		$("#a2").css('opacity',.5);
-				 		aside ="rside";
-				 		atag ="#a2";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[1][2].upper==0){
-				 		$("#b3").css('opacity',.5);
-				 		aside ="upper";
-				 		atag ="#b3";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	return 1;
-				}
-			}
-			else if(dots.charAt(1)=="b"){
-				if(dots.charAt(2)=="1"){
-					if(board[0][0].lower==0){
-				 		$("#a1").css('opacity',.5);
-				 		aside ="lower";
-				 		atag ="#a1";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[2][0].upper==0){
-				 		$("#c1").css('opacity',.5);
-				 		aside ="upper";
-				 		atag ="#c1";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[1][1].lside==0){
-				 		$("#b2").css('opacity',.5);
-				 		aside ="lside";
-				 		atag ="#b2";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	return 1;
-				}
-				else if(dots.charAt(2)=="2"){
-					if(board[0][1].lower==0){
-				 		$("#a2").css('opacity',.5);
-				 		aside ="lower";
-				 		atag ="#a2";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[1][0].rside==0){
-				 		$("#b1").css('opacity',.5);
-				 		aside ="rside";
-				 		atag ="#b1";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[1][2].lside==0){
-				 		$("#b3").css('opacity',.5);
-				 		aside ="lside";
-				 		atag ="#b3";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[2][1].upper==0){
-				 		$("#c2").css('opacity',.5);
-				 		aside ="upper";
-				 		atag ="#c2";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	return 1;
-				}
-				else if(dots.charAt(2)=="3"){
-					if(board[0][2].lower==0){
-				 		$("#a3").css('opacity',.5);
-				 		aside ="lower";
-				 		atag ="#a3";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[1][1].rside==0){
-				 		$("#b2").css('opacity',.5);
-				 		aside ="rside";
-				 		atag ="#b2";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[2][2].upper==0){
-				 		$("#c3").css('opacity',.5);
-				 		aside ="upper";
-				 		atag ="#c3";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	return 1;
-				}
-			}
-			else if(dots.charAt(1)=="c"){
-				if(dots.charAt(2)=="1"){
-					if(board[1][0].lower==0){
-				 		$("#b1").css('opacity',.5);
-				 		aside ="lower";
-				 		atag ="#b1";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[2][1].lside==0){
-				 		$("#c2").css('opacity',.5);
-				 		aside ="lside";
-				 		atag ="#c2";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	return 1;
-				}
-				else if(dots.charAt(2)=="2"){
-					if(board[1][1].lower==0){
-				 		$("#b2").css('opacity',.5);
-				 		aside ="lower";
-				 		atag ="#b2";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[2][0].rside==0){
-				 		$("#c1").css('opacity',.5);
-				 		aside ="rside";
-				 		atag ="#c1";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[2][2].lside==0){
-				 		$("#c3").css('opacity',.5);
-				 		aside ="lside";
-				 		atag ="#c3";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	return 1;
-				}
-				else if(dots.charAt(2)=="3"){
-					if(board[1][2].lower==0){
-				 		$("#b3").css('opacity',.5);
-				 		aside ="lower";
-				 		atag ="#b3";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	if(board[2][1].rside==0){
-				 		$("#c2").css('opacity',.5);
-				 		aside ="rside";
-				 		atag ="#c2";
-				 		available.push({tag:atag,side:aside});
-				 	}
-				 	return 1;
-				}
-				return 0;
-			}
-	// 	};
-	// };
+	var temp = 0;
+
+	if(dots=="#a1"){
+		if(board[0][1].lside==0){
+			$("#a2").css('opacity',.5);
+ 			available.push({tag:"#a2",side:"lside"});
+ 			temp++;
+ 		}
+ 		if(board[1][0].upper==0){
+	 		$("#b1").css('opacity',.5);
+	 		available.push({tag:"#b1",side:"upper"});
+	 		temp++;
+	 	}
+	}
+	else if(dots=="#a2"){
+		if(board[0][0].rside==0){
+	 		$("#a1").css('opacity',.5);
+	 		available.push({tag:"#a1",side:"rside"});
+	 		temp++;
+	 	}
+	 	if(board[0][2].lside==0){
+	 		$("#a3").css('opacity',.5);
+	 		available.push({tag:"#a3",side:"lside"});
+	 		temp++;
+	 	}
+	 	if(board[1][1].upper==0){
+	 		$("#b2").css('opacity',.5);
+	 		available.push({tag:"#b2",side:"upper"});
+	 		temp++;
+	 	}
+	}
+	else if(dots=="#a3"){
+		if(board[0][1].rside==0){
+	 		$("#a2").css('opacity',.5);
+	 		available.push({tag:"#a2",side:"rside"});
+	 		temp++;
+	 	}
+	 	if(board[1][2].upper==0){
+	 		$("#b3").css('opacity',.5);
+	 		available.push({tag:"#b3",side:"upper"});
+	 		temp++;
+	 	}
+	}
+	else if(dots=="#b1"){
+		if(board[0][0].lower==0){
+	 		$("#a1").css('opacity',.5);
+	 		available.push({tag:"#a1",side:"lower"});
+	 		temp++;
+	 	}
+	 	if(board[2][0].upper==0){
+	 		$("#c1").css('opacity',.5);
+	 		available.push({tag:"#c1",side:"upper"});
+	 		temp++;
+	 	}
+	 	if(board[1][1].lside==0){
+	 		$("#b2").css('opacity',.5);
+	 		available.push({tag:"#b2",side:"lside"});
+	 		temp++;
+	 	}
+	}
+	else if(dots=="#b2"){
+		if(board[0][1].lower==0){
+	 		$("#a2").css('opacity',.5);
+	 		available.push({tag:"#a2",side:"lower"});
+	 		temp++;
+	 	}
+	 	if(board[1][0].rside==0){
+	 		$("#b1").css('opacity',.5);
+	 		available.push({tag:"#b1",side:"rside"});
+	 		temp++;
+	 	}
+	 	if(board[1][2].lside==0){
+	 		$("#b3").css('opacity',.5);
+	 		available.push({tag:"#b3",side:"lside"});
+	 		temp++;
+	 	}
+	 	if(board[2][1].upper==0){
+	 		$("#c2").css('opacity',.5);
+	 		available.push({tag:"#c2",side:"upper"});
+	 		temp++;
+	 	}
+	}
+	else if(dots=="#b3"){
+		if(board[0][2].lower==0){
+	 		$("#a3").css('opacity',.5);
+	 		available.push({tag:"#a3",side:"lower"});
+	 		temp++;
+	 	}
+	 	if(board[1][1].rside==0){
+	 		$("#b2").css('opacity',.5);
+	 		available.push({tag:"#b2",side:"rside"});
+	 		temp++;
+	 	}
+	 	if(board[2][2].upper==0){
+	 		$("#c3").css('opacity',.5);
+	 		available.push({tag:"#c3",side:"upper"});
+	 		temp++;
+	 	}
+	}
+	else if(dots=="#c1"){
+		if(board[1][0].lower==0){
+	 		$("#b1").css('opacity',.5);
+	 		available.push({tag:"#b1",side:"lower"});
+	 		temp++;
+	 	}
+	 	if(board[2][1].lside==0){
+	 		$("#c2").css('opacity',.5);
+	 		available.push({tag:"#c2",side:"lside"});
+	 		temp++;
+	 	}
+	}
+	else if(dots=="#c2"){
+		if(board[1][1].lower==0){
+	 		$("#b2").css('opacity',.5);
+	 		available.push({tag:"#b2",side:"lower"});
+	 		temp++;
+	 	}
+	 	if(board[2][0].rside==0){
+	 		$("#c1").css('opacity',.5);
+	 		available.push({tag:"#c1",side:"rside"});
+	 		temp++;
+	 	}
+	 	if(board[2][2].lside==0){
+	 		$("#c3").css('opacity',.5);
+	 		available.push({tag:"#c3",side:"lside"});
+	 		temp++;
+	 	}
+	}
+	else if(dots=="#c3"){
+		if(board[1][2].lower==0){
+	 		$("#b3").css('opacity',.5);
+	 		available.push({tag:"#b3",side:"lower"});
+	 		temp++;
+	 	}
+	 	if(board[2][1].rside==0){
+	 		$("#c2").css('opacity',.5);
+	 		available.push({tag:"#c2",side:"rside"});
+	 		temp++;
+	 	}
+	}
+	return temp;
 }
 
 //IN PROGRESS -- thoughts: counting the sides that create a cell-- issues: if a cell is already claimed
-function check_score(p){
-	
-
-	if(board[0][0].rside && board[0][0].lower && board[1][0].rside && board[1][1].upper){
-
+function check_score(line){
+	var tempcell = new Array;
+	var temp = 0;
+	var flag = false;
+	for(var i = 0;i<4;i++){
+		for(var prop in cells[i]){
+			if(line==cells[i][prop]){
+				tempcell.push(cells[i])
+			}
+		}
 	}
+	for(var i = 0;i<tempcell.length;i++){
+		for(var prop in tempcell[i]){
+			if($(tempcell[i][prop]).css('display')!='none'){
+				temp++;
+			}
+		}
+		if(temp==4){
+			if(player==1){
+				players[0].score++;
+			}
+			else{
+				players[1].score++;
+			}
+			flag=true;
+		}
+		temp=0;
+	}
+	if(flag){
+		$("#p_1").html("Player 1: "+players[0].score)
+		$("#p_2").html("Player 2: "+players[1].score)
+	}
+return flag;
 }
 
 //Perhaps redundant -- 3rd and last block used to be certain a line is to be made.
-function confirm_available(avail){
-	for(var i = 0;i<board.length;i++){
-		for(var j = 0; j<board.length;j++){
-			if(board[i][j].tag==avail.tag){
-				if(board[i][j][avail.side]==0){
-					board[i][j][avail.side]=1;
-					reciprocate(avail);
-					check_score();
-					//drawline(dots);
-					return 1;
-				}
-				return 0;
-			}
+// function confirm_available(avail){
+// 	for(var i = 0;i<board.length;i++){
+// 		for(var j = 0; j<board.length;j++){
+// 			if(board[i][j].tag==avail.tag){
+// 				if(board[i][j][avail.side]==0){
+// 					board[i][j][avail.side]=1;
+// 					reciprocate(avail);
+// 					check_score();
+// 					return 1;
+// 				}
+// 				return 0;
+// 			}
 
-		}
-	}
-}
+// 		}
+// 	}
+// }
 
 //Occupying the side of the first dot(board[i][j]) relative to the second dot(chosen).
 function reciprocate(chosen){
@@ -308,18 +287,22 @@ function reciprocate(chosen){
 function drawline(dots){
 	var dot1 = dots.split("#")
 	var dot2 = prev_choice.split("#")
-	var line_color = (player == 1 ? "#5CE68A" : "#000000")
+	var temp = (player == 1 ? 0 : 1);
+	var line_color = (player == 1 ? players[0].hue : players[1].hue)
 	
 	//if order differs (a1-b1) or (b1-a1)
 	if($("#"+dot1[1]+"-"+dot2[1]).is("div")){
 		$("#"+dot1[1]+"-"+dot2[1]).css('color',line_color)
 		$("#"+dot1[1]+"-"+dot2[1]).fadeToggle('fast');
+		players[temp].lines.push("#"+dot1[1]+"-"+dot2[1])
+		return ("#"+dot1[1]+"-"+dot2[1]);
 	}
 	else if($("#"+dot2[1]+"-"+dot1[1]).is("div")){
 		$("#"+dot2[1]+"-"+dot1[1]).css('color',line_color)
 		$("#"+dot2[1]+"-"+dot1[1]).fadeToggle('fast');
+		players[temp].lines.push("#"+dot2[1]+"-"+dot1[1])
+		return ("#"+dot2[1]+"-"+dot1[1]);
 	}
-	$("#what").html("#"+dot1[1]+"-"+dot2[1]+", "+ line_color)
 }
 
 //created to display the 2-d array of "dots" -- could not make it work through function -- created it in DOM ready.click
@@ -336,6 +319,8 @@ function display(table){
 //pretty much the backbone of selecting dots to making a line
 function select_dots(dots){
 	var confirm = 0;
+	var tempLine = "";
+	var flag = false;
 	for (var i = 0; i < board.length; i++){
 		for(var j = 0; j<board.length;j++){
 			
@@ -345,9 +330,7 @@ function select_dots(dots){
 					//if second choice == first choice:return to normal
 					if(dots==prev_choice){
 						$(dots).css('color','black');
-						$('.left div').css('opacity',1);
-						$('.middle div').css('opacity',1);
-						$('.right div').css('opacity',1);						
+						$('.sort').find("div").css('opacity',1);						
 						choice=0;
 						($("#blah").html()=="Choice 0") ? $("#blah").html("Choice 1") : $("#blah").html("Choice 0");
 					}
@@ -356,21 +339,17 @@ function select_dots(dots){
 						
 						//if found
 						if(dots==available[k].tag){	
-							
-							//make sure
-							confirm=confirm_available(available[k],board[i][j]);
-							
-							//create line & switch player & reset
-							if (confirm){
-								drawline(dots);
-								$(dots).css('color','black');
-								$(prev_choice).css('color','black')
-								choice=0;
-								($("#blah").html()=="Choice 0") ? $("#blah").html("Choice 1") : $("#blah").html("Choice 0");
-								$('.left div').css('opacity',1);
-								$('.middle div').css('opacity',1);
-								$('.right div').css('opacity',1);
-								switchplayer();							
+								// $("#what").html(available[k].side)
+							board[i][j][available[k].side]=1;
+							reciprocate(available[k])
+							tempLine=drawline(dots);
+							flag=check_score(tempLine);
+							$(prev_choice).css('color','black')
+							choice=0;
+							($("#blah").html()=="Choice 0") ? $("#blah").html("Choice 1") : $("#blah").html("Choice 0");
+							$('.sort').find("div").css('opacity',1);
+							if(!flag){
+								switchplayer();	
 							}
 						}
 					}
@@ -386,7 +365,8 @@ function select_dots(dots){
 						
 						//finds available dots: returns 1/0 to break loop
 						confirm=find_available(dots);
-						if (confirm){
+						// $('#what').html(available[0].side+","+available[1].side)
+						if (confirm>0){
 							$(dots).css('color','#E62E00');
 							prev_choice = dots;
 							choice=1;
@@ -400,6 +380,63 @@ function select_dots(dots){
 	};
 }
 
+function changecolor(newcolor,whichPlayer){
+	players[whichPlayer].hue=newcolor;
+
+	for(var j = 0; j<players[whichPlayer].lines.length;j++){
+		$(players[whichPlayer].lines[j]).css('color',newcolor)
+	}
+
+}
+
+function check_color(){
+	var temp = "";
+	$('#choosecolor1').children('p').each(function(){
+		temp = $(this).css('background-color')
+		if(temp==$('#color2').css('background-color')){
+			$(this).css('display','none');
+		}
+		else{
+			$(this).css('display','block')
+		}
+	});
+	$('#choosecolor2').children('p').each(function(){
+		temp=$(this).css('background-color')
+		if(temp==$('#color1').css('background-color')){
+			$(this).css('display','none');
+			return 0;
+		}
+		else{
+			$(this).css('display','block')
+		}
+	});
+
+}
+
+function match_cells(){
+	var array = new Array;
+	array[0]= new block("#a1-a2","#a2-b2","#b1-b2","#a1-b1");
+	array[1]= new block("#a2-a3","#a3-b3","#b2-b3","#a2-b2");
+	array[2]= new block("#b1-b2","#b2-c2","#c1-c2","#b1-c1");
+	array[3]= new block("#b2-b3","#b3-c3","#c2-c3","#b2-c2");
+	return array;
+}
+
+function block(u,r,d,l){
+	this.u=u;
+	this.r=r;
+	this.d=d;
+	this.l=l;
+}
+
+function person(num,hue){
+	this.num=num;
+	this.score=0;
+	this.lines=new Array();
+	this.hue=hue;
+	this.handcock="";
+}
+
 //global variables
 var board = create_T(3);
 var player = 1;
@@ -408,15 +445,64 @@ var prev_choice = "";
 var available= [];
 var play1_score=0;
 var play2_score=0;
+var name1="";
+var name2="";
 
+var players = [new person(1,"green"),new person(2,"black")]
+var cells = match_cells();
 
 $(document).ready(function(){
-	$("body").click(function(event){
+	
+	players[0].handcock=prompt("Player1: Name?");
+	players[1].handcock=prompt("Player2: Name?");
+
+
+
+	$('body').click(function(event){
+		$("#font_color").html(event.target.id + event.target.className)
+	})
+
+	$('.score').click(function(event){
+		check_color();
+		if(event.target.id=="color1"){
+			$('#choosecolor1').slideToggle('fast');
+
+			if($('#choosecolor2').css('display')!='none'){
+				$('#choosecolor2').slideToggle('fast');
+			}
+		}
+		else if(event.target.id=='color2'){
+			$("#choosecolor2").slideToggle('fast');
+			if($('#choosecolor1').css('display')!='none'){
+				$('#choosecolor1').slideToggle('fast');
+			}
+		}
+		
+	});
+
+	$('#colors').click(function(event){
+		var tempcolor = $(event.target).css('background-color');
+
+		if($(event.target).closest('div').attr('id')=='choosecolor1'){
+			$('#color1').css('background-color',tempcolor)
+			changecolor(tempcolor,0);
+		}
+		else{
+			$('#color2').css('background-color',tempcolor)
+			changecolor(tempcolor,1)
+		}
+	});
+
+
+	$(".arena").click(function(event){
 		var pos = event.target.id
 		var code = pos.split("");
 
+		if($("#choosecolor1").css('display')!='none'){$('#choosecolor1').slideToggle('fast')}
+		else if($('#choosecolor2').css('display')!='none'){$('#choosecolor2').slideToggle('fast')}
 		select_dots("#"+pos);
 
+		
 
 		$("#error").html("");
 
@@ -435,6 +521,7 @@ $(document).ready(function(){
 
 
 	});
+
 });
 
 // function begin(){
